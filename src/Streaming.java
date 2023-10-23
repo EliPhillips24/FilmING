c9cc9c1189dfb0a665c63663babecfeebe4226ff
kingfisheii <132377565+kingfisheii@users.noreply.github.com>
2023-10-23 13:46:39 -0400

    I done something

--- src/Streaming.java
+++ src/Streaming.java
@@ -2,29 +2,28 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class Streaming {
-
-    private LocalDate Launch;
+   // private LocalDate Launch;
     private String service;
     private int Subscribers;
-    private String Content;
+  //  private String Content;
+    private String Owner;
+
     private Date release;
-
-
-    public Streaming(LocalDate launch, String service, int subscribers, String content ) {
-        Launch = launch;
+   // private String Place;
+    public Streaming(String service, int subscribers, String owner) {
+      //  Launch = launch;
         this.service = service;
         Subscribers = subscribers;
-        Content = content;
-    }
-/*
-    public Date getRelease() {
-        return release;
+      //  Content = content;
+        Owner = owner;
+      //  Place = place;
+
     }
 
-    public void setRelease(Date release) {w
-        this.release = release;
+    public Streaming() {
+
     }
-*/
+
     public String getService() {
         return service;
     }
@@ -41,17 +40,33 @@ public class Streaming {
         Subscribers = subscribers;
     }
 
-    public String getContent() {
-        return Content;
+   // public String getContent() {
+     //   return Content;
+  //  }
+
+  //  public void setContent(String content) {
+     //   Content = content;
+ //   }
+    //public String getPlace() {
+    //    return Place;
+    //}
+
+    public void setPlace(String Place) {
+        Place = Place;
+    }
+    public String getOwner() {
+        return Owner;
     }
 
-
-    public void setContent(String content) {
-        Content = content;
+    public void setOwner(String Owner) {
+        Owner = Owner;
     }
 
+   // public String toString() {
+   //     return "Name Of Service" + getService()+ "Launched On: " + Launch +" With Currently "+ getSubscribers()/1000000+ " Million Subscribers" + " and streams" + getContent() + getPlace()+ getOwner();
+ //   }
 
-    public String toString() {
-        return "Name Of Service" + getService()+ "Launched On: " + Launch +" With Currently "+ getSubscribers()+ "Subscribers" + " and streams" + getContent() ;
-    }
+
 }
+//if LoctionOfSquareBracket is greatier thann zero
+//int locationOfSquareBracket
